3f4ca8d15ecbec62f52f2be9e218026920c77324
Sam Mckay <sam.mckay@aol.co.uk>
2016-02-08 10:02:00 +0000

    fixed redirect from empty cart to shop front, renamed front-page.php the woocommerce.php and changed the shop page in wp-admin

--- woocommerce/cart/cart-empty.php
+++ woocommerce/cart/cart-empty.php
@@ -9,28 +9,29 @@
  * as little as possible, but it does happen. When this occurs the version of the template file will.
  * be bumped and the readme will list any important changes.
  *
- * @see 	    http://docs.woothemes.com/document/template-structure/
+ * @see        http://docs.woothemes.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 2.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined ( 'ABSPATH' )) {
+    exit; // Exit if accessed directly
 }
 
-wc_print_notices();
+wc_print_notices ();
 
 ?>
 
 <p class="cart-empty">
-	<?php _e( 'Your cart is currently empty.', 'woocommerce' ) ?>
+    <?php _e ( 'Your cart is currently empty.' , 'woocommerce' ) ?>
 </p>
 
-<?php do_action( 'woocommerce_cart_is_empty' ); ?>
+<?php do_action ( 'woocommerce_cart_is_empty' ); ?>
 
 <p class="return-to-shop">
-	<a class="button wc-backward" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
-		<?php _e( 'Return To Shop', 'woocommerce' ) ?>
-	</a>
+    <a class="button wc-backward"
+       href="<?php echo esc_url ( apply_filters ( 'woocommerce_return_to_shop_redirect' , wc_get_page_permalink ( 'shop' ) ) ); ?>">
+        <?php _e ( 'Return To Shop' , 'woocommerce' ) ?>
+    </a>
 </p>
