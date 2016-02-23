93488a4fd95f6bd335076f52ade228e590ef6877
Sam Mckay <sam.mckay@aol.co.uk>
2016-02-23 16:13:07 +0000

    moved main css file to enqueue, not header

--- functions.php
+++ functions.php
@@ -7,13 +7,13 @@ function woocommerce_support ()
     add_theme_support ( 'woocommerce' );
 }
 
-// remove woocommerce stylingz
+// remove woocommerce styling
 add_filter ( 'woocommerce_enqueue_styles' , '__return_empty_array' );
 
 // styles and scripts
 function theme_scripts ()
 {
-    // wp_enqueue_style( 'main-css', get_template_directory_uri() . '/assets/stylesheets/main.css' );
+    wp_enqueue_style( 'main-css', get_template_directory_uri() . '/assets/stylesheets/main.css' );
     wp_enqueue_script (
         'main-js' , get_template_directory_uri () . '/assets/javascripts/build.js' , array ( 'jquery' ) , '1.0.0' , true
     );
