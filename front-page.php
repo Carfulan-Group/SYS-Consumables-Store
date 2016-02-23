af80b91023ad73c459d1800dd8aa5cd3e99f9e47
Sam Mckay <sam.mckay@aol.co.uk>
2016-02-23 15:03:53 +0000

    conditional sections on homepage if user has correct permissions or not

--- front-page.php
+++ front-page.php
@@ -1,42 +1,75 @@
 <?php
 get_header ();
 
-if (is_user_logged_in ()) {
-    echo "<h1 class='page-title'>Hello " . $current_user->user_firstname
-         . ", here's a list of compatible products:</h1>";
+if (is_user_logged_in ())
+// // //
+// check if user is logged in
+// // //
+{
     ?>
-    <section id="home-products" class="vertical-padding-large">
+
         <?php
-        wc_print_notices ();
+        require_once( ABSPATH . 'wp-includes/pluggable.php' );
+        $groups_user = new Groups_User( get_current_user_id() );
+
+        if ( $groups_user->can( 'My Consumables' ) )
+        // // //
+        // if users has access to My Consumables group
+        // // //
+        {
+            echo "<h1 class='page-title'>Hello " . $current_user->user_firstname
+                 . ", here's a list of compatible products:</h1>";
+                 wc_print_notices ();
+            ?>
+            <section id="home-products" class="vertical-padding-large">
+            <div class="home-cat-selector">
+                <ul>
+                    <li class="active" onclick="catSelect(this)" data-tab="all-cats">All</li>
+                    <li onclick="catSelect(this)" data-tab="model-cat">Model Materials</li>
+                    <li onclick="catSelect(this)" data-tab="support-cat">Support</li>
+                    <li onclick="catSelect(this)" data-tab="hardware-cat">Hardware</li>
+                </ul>
+            </div>
+            <div class="home-search">
+                <input type="text" placeholder="Search" onkeyup="itemSearch(this)">
+            </div>
+            <div class="home-cat-container">
+                <div class="home-cat all-cats model-cat">
+                    <h2>Model Materials</h2>
+                    <?php echo do_shortcode ( '[product_category category="model material"]' ); ?>
+                </div>
+                <div class="home-cat all-cats support-cat">
+                    <h2>Support Materials</h2>
+                    <?php echo do_shortcode ( '[product_category category="support material"]' ); ?>
+                </div>
+                <div class="home-cat all-cats hardware-cat">
+                    <h2>Printing Hardware</h2>
+                    <?php echo do_shortcode ( '[product_category category="hardware"]' ); ?>
+                </div>
+            </div>
+            <?php
+        }
+
+        else
+        // // //
+        // if user account has not been set up yet
+        // // //
+        {
+            echo "<h1 class='page-title'>Hello " . $current_user->user_firstname . "</h1>";
+            wc_print_notices ();
+            echo '<section id="home-products" class="vertical-padding-large">';
+            echo "<p>Our team is busy tailoring your account to your needs and equipment. You will be alerted when we are done, thank you for your patience.</p>";
+        }
         ?>
-        <div class="home-cat-selector">
-            <ul>
-                <li class="active" onclick="catSelect(this)" data-tab="all-cats">All</li>
-                <li onclick="catSelect(this)" data-tab="model-cat">Model Materials</li>
-                <li onclick="catSelect(this)" data-tab="support-cat">Support</li>
-                <li onclick="catSelect(this)" data-tab="hardware-cat">Hardware</li>
-            </ul>
-        </div>
-        <div class="home-search">
-            <input type="text" placeholder="Search" onkeyup="itemSearch(this)">
-        </div>
-        <div class="home-cat-container">
-            <div class="home-cat all-cats model-cat">
-                <h2>Model Materials</h2>
-                <?php echo do_shortcode ( '[product_category category="model material"]' ); ?>
-            </div>
-            <div class="home-cat all-cats support-cat">
-                <h2>Support Materials</h2>
-                <?php echo do_shortcode ( '[product_category category="support material"]' ); ?>
-            </div>
-            <div class="home-cat all-cats hardware-cat">
-                <h2>Printing Hardware</h2>
-                <?php echo do_shortcode ( '[product_category category="hardware"]' ); ?>
-            </div>
-        </div>
-    </section>
-    <?php
-} else {
+        </section>
+        <?php
+}
+
+else
+// // //
+// if user isn't logged in
+// // //
+{
     ?>
     <h1 class="page-title">SYS Systems Consumables Store</h1>
     <section id="home-login" class="vertical-padding-large">
