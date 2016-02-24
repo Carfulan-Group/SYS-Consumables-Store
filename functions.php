eab436603a12a4b6f53685190091593d5055bb8a
Sam Mckay <sam.mckay@aol.co.uk>
2016-02-24 12:27:31 +0000

    changed WooCommerce redirect after registration to home page

--- functions.php
+++ functions.php
@@ -1,16 +1,21 @@
 <?php
-
+// // //
 // declare woocommerce support
+// // //
 add_action ( 'after_setup_theme' , 'woocommerce_support' );
 function woocommerce_support ()
 {
     add_theme_support ( 'woocommerce' );
 }
 
+// // //
 // remove woocommerce styling
+// // //
 add_filter ( 'woocommerce_enqueue_styles' , '__return_empty_array' );
 
-// styles and scripts
+// // //
+// enqueue styles and scripts
+// // //
 function theme_scripts ()
 {
     wp_enqueue_style( 'main-css', get_template_directory_uri() . '/assets/stylesheets/main.css' );
@@ -21,11 +26,24 @@ function theme_scripts ()
 
 add_action ( 'wp_enqueue_scripts' , 'theme_scripts' );
 
-// menus
+// // //
+// register nav menus
+// // //
 register_nav_menu ( 'main-menu' , 'Main Menu' );
 register_nav_menu ( 'footer-menu' , 'Footer Menu' );
 register_nav_menu ( 'footer-quick-links' , 'Footer Quick Links' );
 register_nav_menu ( 'lower-footer-menu' , 'Lower Footer Menu' );
 
-
+// // //
+// remove cross-sells from cart page
+// // //
 remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' , 10 );
+
+// // //
+// redirect to homepage after user registration
+// // //
+add_filter('woocommerce_registration_redirect', 'ps_wc_registration_redirect');
+function ps_wc_registration_redirect( $redirect_to ) {
+     $redirect_to = site_url();
+     return $redirect_to;
+}
