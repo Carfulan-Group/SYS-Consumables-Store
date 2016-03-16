273812eebe81402670a695a8514741072f3b24bf
Sam Mckay <sam.mckay@aol.co.uk>
2016-03-16 09:49:09 +0000

    changed filter by machine to just filter as now contains materials for FDM filtering

--- front-page.php
+++ front-page.php
@@ -32,7 +32,7 @@ if (is_user_logged_in ())
             </div>
             <div class="home-search">
                 <select class="home-search-select">
-                    <option selected="selected" value="0">By Machine</option>
+                    <option selected="selected" value="0">Filter</option>
                 </select>
                 <input type="text" placeholder="Search" onkeyup="itemSearch(this)">
             </div>
