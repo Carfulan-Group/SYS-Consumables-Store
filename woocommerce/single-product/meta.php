3be9c895333b9e7cce455718045bf1705ba0d36c
Sam Mckay <sam.mckay@aol.co.uk>
2016-03-23 11:09:46 +0000

    added in get_available_machines_options

--- woocommerce/single-product/meta.php
+++ woocommerce/single-product/meta.php
@@ -38,7 +38,9 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php echo $product->get_categories( ', ', '<p><strong>' . _n( 'Category : </strong>', 'Categories : </strong>', $cat_count, 'woocommerce' ) , '</p>' ); ?>
 
-	<?php echo $product->get_tags( ', ', '<p><strong>' . _n( 'Tags : </strong>', 'Tags : </strong>', $tag_count, 'woocommerce' ) , '</p>' ); ?>
+	<p><strong>Compatible : </strong>
+	<?php get_available_machines_options($type = 'span'); ?>
+	</p>
 
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
 
