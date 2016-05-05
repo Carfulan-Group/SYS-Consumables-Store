95a69c3611845d5738ef994df72abb5249ea1714
Sam Mckay <sam.mckay@aol.co.uk>
2016-05-05 09:11:04 +0100

    Tweaked user new account email so that they know we are setting there account up for them prior the them using it

--- woocommerce/emails/customer-new-account.php
+++ woocommerce/emails/customer-new-account.php
@@ -1,38 +1,43 @@
 <?php
-/**
- * Customer new account email
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-new-account.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
- * will need to copy the new files to your theme to maintain compatibility. We try to do this.
- * as little as possible, but it does happen. When this occurs the version of the template file will.
- * be bumped and the readme will list any important changes.
- *
- * @see 	    http://docs.woothemes.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates/Emails
- * @version     1.6.4
- */
+	/**
+	 * Customer new account email
+	 *
+	 * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-new-account.php.
+	 *
+	 * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+	 * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+	 * as little as possible, but it does happen. When this occurs the version of the template file will.
+	 * be bumped and the readme will list any important changes.
+	 *
+	 * @see        http://docs.woothemes.com/document/template-structure/
+	 * @author        WooThemes
+	 * @package    WooCommerce/Templates/Emails
+	 * @version     1.6.4
+	 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+	if ( !defined ( 'ABSPATH' ) )
+	{
+		exit; // Exit if accessed directly
+	}
 
 ?>
 
-<?php do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+<?php do_action ( 'woocommerce_email_header', $email_heading, $email ); ?>
 
-<p><?php printf( __( "Thanks for creating an account on %s. Your username is <strong>%s</strong>.", 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
+<p>Thanks for registering on our consumables store,</p>
 
-<?php if ( 'yes' === get_option( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
+<p>If you're seeing this and don't remember signing up for an account don't be alarmed! We set up accounts for all of our new customers so we can tailor them to the machines you own and ensure you don't purchase the wrong parts.</p>
 
-	<p><?php printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce' ), esc_html( $user_pass ) ); ?></p>
+<p>You can visit the store
+	<a href="<?php echo site_url (); ?>">here</a> and log in using your email address and the password below:</p>
+
+
+<?php if ( 'yes' === get_option ( 'woocommerce_registration_generate_password' ) && $password_generated ) : ?>
+
+	<p><?php printf ( __ ( "Password: <strong>%s</strong>", 'woocommerce' ), esc_html ( $user_pass ) ); ?></p>
 
 <?php endif; ?>
 
-<p><?php printf( __( 'You can access your account area to view your orders and change your password here: %s.', 'woocommerce' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
+<?php do_action ( 'woocommerce_email_footer', $email ); ?>
 
-<?php do_action( 'woocommerce_email_footer', $email ); ?>
-
-<?php do_action( 'new_customer_registered', $user_login ); ?>
+<?php do_action ( 'new_customer_registered', $user_login ); ?>
