fd1bc04d30d846514d46b1db856b7878a66eff8a
Sam Mckay <sam.mckay@aol.co.uk>
2016-05-06 15:57:53 +0100

    IMPROVEMENTS: Category page now works with new product loop

--- woocommerce/taxonomy-product_cat.php
+++ woocommerce/taxonomy-product_cat.php
@@ -1,21 +1,36 @@
 <?php
-/**
- * The Template for displaying products in a product category. Simply includes the archive template
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/taxonomy-product_cat.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
- * will need to copy the new files to your theme to maintain compatibility. We try to do this.
- * as little as possible, but it does happen. When this occurs the version of the template file will.
- * be bumped and the readme will list any important changes.
- *
- * @see 	    http://docs.woothemes.com/document/template-structure/
- * @package 	WooCommerce/Templates
- * @version     1.6.4
- */
+	/**
+	 * The Template for displaying products in a product category. Simply includes the archive template
+	 *
+	 * This template can be overridden by copying it to yourtheme/woocommerce/taxonomy-product_cat.php.
+	 *
+	 * HOWEVER, on occasion WooCommerce will need to update template files and you (the theme developer).
+	 * will need to copy the new files to your theme to maintain compatibility. We try to do this.
+	 * as little as possible, but it does happen. When this occurs the version of the template file will.
+	 * be bumped and the readme will list any important changes.
+	 *
+	 * @see        http://docs.woothemes.com/document/template-structure/
+	 * @package    WooCommerce/Templates
+	 * @version     1.6.4
+	 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+	if ( !defined ( 'ABSPATH' ) )
+	{
+		exit; // Exit if accessed directly
+	}
 
-wc_get_template( 'archive-product.php' );
+	get_header ();
+	$cat_name = single_cat_title ( '', false );
+?>
+	<div class="page-title">
+		<h1><?php echo $cat_name; ?></h1>
+	</div>
+	<div class="white vertical-padding">
+<?php
+
+	sysProductLoop ( $cat_name, -1 );
+?>
+	<div>
+<?php
+	get_footer ();
+?>
